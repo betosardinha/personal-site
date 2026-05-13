9739d4798bbdf8d3a457f1b7c7986dc924608df9
Alberto Sardinha <alberto.sardinha@justifi.tech>
2026-05-13 15:02:07 -0300

    feat: add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import './layout.css';
+  import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <title>Beto Sardinha - Developer</title>
+  <meta name="description" content="Personal website" />
+</svelte:head>
+
 {@render children()}
